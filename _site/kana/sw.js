6b7e6caedbc3ea82887daa6ee69bddffc0c5bddc
0xb01 <johnnichols.ranara@gmail.com>
2026-03-17 15:55:04 +0800

    Offline support, PWA

---
+++ _site/kana/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'kana-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install: Cache everything we need
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate: Clean up old caches if we update the version
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Try the cache first, then the network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
